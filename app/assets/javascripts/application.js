455ff279775fa7f6ef13b852acf9b5c7f87dd6f7
Daniel <danwa5@gmail.com>
2013-12-10 12:00:36 -0500

    Minor bug fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-
-//= require jquery
 //= require bootstrap
+//= require jquery
+
 //= require jquery_ujs
 //= require jquery.raty
 //= require turbolinks
